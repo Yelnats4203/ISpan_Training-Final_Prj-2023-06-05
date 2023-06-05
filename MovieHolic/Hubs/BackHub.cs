5221e5f5e923d84ac48e278beab2bd48578a93b2
Stanley <stanley160783@gmail.com>
2023-06-05 15:15:06 +0800

    更新Hubs

---
+++ MovieHolic/Hubs/BackHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics.Metrics;
+
+namespace prjMovieHolic.Hubs
+{
+    public class BackHub : Hub
+    {
+
+        public long getCounter()  //後檯登入時取得當下counter
+        {
+            return FrontHub.counter;
+        }
+
+    }
+}
---
+++ MovieHolic/Hubs/FrontHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace prjMovieHolic.Hubs
+{
+    public class FrontHub:Hub
+    {
+        public static long counter=0;
+
+        private readonly IHubContext<BackHub> _backHub;
+
+        public FrontHub(IHubContext<BackHub> backHub)
+        {
+            _backHub = backHub;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            counter = counter + 1;//當用戶進入頁面在線人數加一
+            //await Clients.All.SendAsync("updateCounter", counter); //登入刷新人數
+            await _backHub.Clients.All.SendAsync("updateCounter", counter);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? e)
+        {
+            counter = counter - 1;
+            //await Clients.All.SendAsync("updateCounter", counter);
+            await _backHub.Clients.All.SendAsync("updateCounter", counter);
+            await base.OnDisconnectedAsync(e);
+        }
+
+        //public long getCounter()  //後檯登入時取得當下counter
+        //{
+        //    return counter;
+        //}
+    }
+}
